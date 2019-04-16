d4730769283abf5422349893f0c77773d97d79f4
Romain GONÇALVES <romain.goncalves@gmail.com>
2019-04-16 19:42:04 +0200

    Updates installation method for CKEditor to use Webpack instead of file download ; Adds IFAO Greek font to CKEditor

--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,6 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     //.addEntry('page2', './assets/js/page2.js')
-    .addStyleEntry('ckeditor', './assets/css/ckeditor.css')
+    .addStyleEntry('fonts', './assets/css/fonts.css')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
@@ -30,6 +30,13 @@ Encore
         to: 'images/[path][name].[ext]',
         pattern: /\.(png|jpg|jpeg)$/
     })
+    .copyFiles([
+        { from: './node_modules/ckeditor/', to: 'ckeditor/[path][name].[ext]', pattern: /\.(js|css)$/, includeSubdirectories: false },
+        { from: './node_modules/ckeditor/adapters', to: 'ckeditor/adapters/[path][name].[ext]' },
+        { from: './node_modules/ckeditor/lang', to: 'ckeditor/lang/[path][name].[ext]' },
+        { from: './node_modules/ckeditor/plugins', to: 'ckeditor/plugins/[path][name].[ext]' },
+        { from: './node_modules/ckeditor/skins', to: 'ckeditor/skins/[path][name].[ext]' }
+    ])
 
     /*
      * FEATURE CONFIG

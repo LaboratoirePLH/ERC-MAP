21a21e82ba6373747a7dfe01d885e68414f7de0a
Romain GONÇALVES <romain.goncalves@gmail.com>
2019-05-14 11:26:20 +0200

    Increases font-size in WYSIWYG editors

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ Encore
     .addEntry('app', './assets/js/app.js')
     //.addEntry('page2', './assets/js/page2.js')
     .addStyleEntry('fonts', './assets/css/fonts.css')
+    .addStyleEntry('ckeditorcustom', './assets/css/ckeditorcustom.css')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app

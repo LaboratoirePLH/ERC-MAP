97d531ee0cced54d1571adf53f157b9f1313f6ca
Aleridia <maxdu34.m@gmail.com>
2019-05-23 16:23:48 +0100

    Moteur de recherche v1

--- src/Controller/RequetesController.php
+++ src/Controller/RequetesController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; //Remplace le 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\File\Exception\NoFileException;
 
 use App\Entity \ {
     Chercheur,
@@ -117,27 +118,35 @@ class RequetesController extends AbstractController
     {
         if ($request->isXmlHttpRequest()) {
             $nomBDD = $request->request->get('nomBDD');
+
+            //Traitement de nomBDD, pour cela corresponde au getNom($lang) dans le switch
+            if (strcmp($nomBDD, "nom_fr") == 0) {
+                $nomBDD = "fr";
+            } elseif (strcmp($nomBDD, "nom_en") == 0) {
+                $nomBDD = "en";
+            }
+
             $nomTable = $request->request->get('nomTable');
-            $nomTableClass = "App\\Entity\\".$nomTable; //On le traduit en Symfony pour qu'il comprenne où aller
+            $nomTableClass = "App\\Entity\\" . $nomTable; //On le traduit en Symfony pour qu'il comprenne où aller
 
             //Requête QB
             $repo = $this->getDoctrine()->getManager()->getRepository($nomTableClass);
             $rows = $repo->createQueryBuilder("e")
                 ->getQuery()
                 ->getResult();
-            $responseArray = array();
-            
-            return new JsonResponse($this->_traiterRetour($rows,$responseArray,$nomBDD));
+
+            return new JsonResponse($this->_traiterRetour($rows, $nomBDD));
         }
     }
 
-    
+
     /**
      * Page queries
      *
      * @Route("/requetes/chargerReq", name="requetes_charger_requete", options={"expose"=true})
      */
-    public function chargerRequete(Request $request){
+    public function chargerRequete(Request $request)
+    {
         if ($request->isXmlHttpRequest()) {
             $idReq = $request->request->get('idReq');
 
@@ -149,8 +158,8 @@ class RequetesController extends AbstractController
                 ->getQuery()
                 ->getResult();
 
-                $corpsReq = $corpsReq[0]->getReqCorps();
-                return new JsonResponse($corpsReq);
+            $corpsReq = $corpsReq[0]->getReqCorps();
+            return new JsonResponse($corpsReq);
         }
     }
 
@@ -159,63 +168,322 @@ class RequetesController extends AbstractController
      *
      * @Route("/requetes/executerReq", name="requetes_executer", options={"expose"=true})
      */
-    public function executerRequete(Request $request){
+    public function executerRequete(Request $request)
+    {
         if ($request->isXmlHttpRequest()) {
-          //  $typeDonnee = $request->request->get('typeDonnee');
-            //$tabRequete = $request->request->get('tabRequete');
-           // $tabAffiche = $request->request->get('tabAffiche');
+            $typeDonnee = strtolower($request->request->get('typeDonnee')); //Je le formate pour que ça corresponde aux tables de la BDD
+            $tabRequete = $request->request->get('tabRequete');
+            //$tabAffiche = $request->request->get('tabAffiche');
             //$typeDonneeTable = "App\\Entity\\".$typeDonnee; //On le traduit en Symfony pour qu'il comprenne où aller
 
-            $typeDonneeTable = "App\\Entity\\Requetes";
-            //QB
-            $query = $this->getDoctrine()->getManager()->createQueryBuilder();
-            $query->select([
-                
-                
-            ])
-            ->from($typeDonneeTable, 's')
-            
-            ->getQuery();
+            $nbCriteres = sizeof($tabRequete);
 
-            $tmp="oui";
-            return new JsonResponse($query);
+            //Affectation du contenu de tabRequete         
+            $tabTable = array();
+            $tabNomBDD = array();
+            $tabValue = array();
+            $tabOperator = array();
+            for ($i = 0; $i < $nbCriteres; $i++) {
+                $tabTable[$i] = $tabRequete["where" . $i]["table"];
+                $tabNomBDD[$i] = $tabRequete["where" . $i]["nomBDD"];
+                $tabValue[$i] = $tabRequete["where" . $i]["value"];
+                $tabOperator[$i] = $tabRequete["where" . $i]["operator"];
+            }
+            $tabTable = array_unique($tabTable, SORT_REGULAR); //J'enlève les doublons
+
+            //Pour les jointures  
+            $jointures = " WHERE 1=1 ";
+            $jointures .= $this->_faireJointure($typeDonnee, $tabTable);
+
+            //Pour le FROM WHERE
+            $from = "";
+            $from .= $this->_faireFrom($from, $tabTable, $typeDonnee);
+
+            $condition = ""; //Where
+
+            for ($i = 0; $i < sizeof($tabTable); $i++) {
+                $condition .= "AND " . $tabTable[$i] . "." . $tabNomBDD[$i] . " ";
+                $condition .= $this->_operatorValue($tabOperator[$i], $tabValue[$i]);
+            }
+
+            $sql = "SELECT attestation.id ";
+            $sql .= $from;
+            $sql .= $jointures;
+            $sql .= $condition;
+
+            //return new JsonResponse($sql);
+
+            //Requête
+            $conn = $this->getDoctrine()->getEntityManager()->getConnection();
+
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+
+            //Traitement de la requête
+            $tmp2 = [];
+            $i = 0;
+            while ($data = $stmt->fetch()) {
+                $tmp2[$i]["id"] = $data["id"];
+                $i++;
+            }
+            return new JsonResponse($tmp2);
         }
     }
 
-    private function _traiterRetour($rows, $reponseArray,$nomBDD){//Pour ceux à qui la méthode getNom() ne peut s'appliquer
-        switch($nomBDD){
-            case "nomVille":
-                foreach ($rows as $row) {
-                    if($row->getNomVille() == "null" || $row->getNomVille() == ""){ //Pour virer ce qui n'est pas rempli/null
+    private function _faireFrom($from, $tabTable, $typeDonnee)
+    {
+        if (in_array($typeDonnee, $tabTable)) { //Si le type donnée est dans le tableau
+            $from = "FROM "; //From
+        } else { //S'il n'y est pas on l'ajoute pour pouvoir faire la requête
+            $from = "FROM " . $typeDonnee . ", ";
+        }
 
+        for ($i = 0; $i < sizeof($tabTable); $i++) {
+            switch($tabTable[$i]){
+                case "categorie_materiel":
+                    if(!in_array("materiel",$tabTable)){ //S'il n'est pas dans le tableau (Pour ne pas l'ajouter deux fois)
+                        $from .= "materiel, ";
+                        if(!in_array("attestation_materiel",$tabTable)){
+                            $from .= "attestation_materiel, ";
+                        }
                     }
-                    else{
+                    break;
+                
+                case "materiel":
+                    if(!in_array("attestation_materiel",$tabTable)){ //S'il n'est pas dans le tableau (Pour ne pas l'ajouter deux fois)
+                        $from .= "attestation_materiel, ";
+                    }
+                    break;
+
+                case "occasion":
+                    if(!in_array("attestation_occasion",$tabTable)){ //S'il n'est pas dans le tableau (Pour ne pas l'ajouter deux fois)
+                        $from .= "attestation_occasion, ";
+                    }
+                    break;
+
+                case "categorie_occasion":
+                    if(!in_array("occasion",$tabTable)){ //S'il n'est pas dans le tableau (Pour ne pas l'ajouter deux fois)
+                        $from .= "occasion, ";
+                        if(!in_array("attestation_occasion",$tabTable)){
+                            $from .= "attestation_occasion, ";
+                        }
+                    }
+                    break;
+            }
+            $from .= $tabTable[$i];
+            if ($i + 1 != sizeof($tabTable)) { //Pour ne pas mettre la virgule au dernier
+                $from .= ", ";
+            }
+        }
+        return $from;
+    }
+    private function _faireJointure($typeDonnee, $tabTable)
+    {
+        $jointures = "";
+        foreach ($tabTable as $table) {
+            if ($table == "localisation") {
+                if ($typeDonnee == "source") { //Si c'est sur la source, c'est un traitement différent
+
+                } else { //Pour l'élément et l'attestation
+                    $jointures .= "AND " . $typeDonnee . ".id_localisation = localisation.id ";
+                }
+            } else {
+                switch ($typeDonnee) {
+                    case "attestation":
+                        switch ($table) {
+                                //Les périphériques à Attestation
+                            case "attestation": //Si c'est la même table
+                                break;
+
+                            case "etat_fiche":
+                                $jointures .= "AND attestation.id_etat_fiche = etat_fiche.id ";
+                                break;
+
+                            case "categorie_occasion":
+                                $tabFaireJointureTmp[0] = "occasion";
+                                $jointures .= $this->_faireJointure($typeDonnee, $tabFaireJointureTmp); //J'appelle récursivement la fonction, pour qu'il me renvoir la jointure que je concatène
+                                $jointures .= "AND occasion.categorie_occasion_id = categorie_occasion.id ";
+                                break;
+
+                            case "occasion":
+                                if(!in_array("categorie_occasion",$tabTable)){ //Pour éviter de le mettre deux fois
+                                    $jointures .= "AND attestation_occasion.id_attestation = attestation.id AND attestation_occasion.id_occasion = occasion.id ";
+                                }
+                                break;
+
+                            case "categorie_materiel":
+                                $tabFaireJointureTmp[0] = "materiel";
+                                $jointures .= $this->_faireJointure($typeDonnee, $tabFaireJointureTmp);
+                                $jointures .= "AND materiel.id = categorie_materiel.id ";
+                                break;
+
+                            case "materiel":
+                                if(!in_array("categorie_materiel",$tabTable)){ //Pour éviter de le mettre deux fois
+                                    $jointures .= "AND attestation_materiel.id_attestation = attestation.id AND attestation_materiel.id_materiel = materiel.id ";
+                                }
+                                break;
+
+                            case "source":
+                                $jointures .= "AND attestation.id_source = source.id ";
+                                break;
+
+                            case "agent":
+                                $jointures .= "AND attestation.id = agent.id_attestation ";
+                                break;
+
+                            case "element":
+                                $jointures .= "AND attestation.id = contient_element.id_attestation AND contient_element.id_element = element.id ";
+                                break;
+
+                            default:
+                                throw new NoFileException('Champ non pris en compte');
+                                break;
+                        }
+                        break;
+
+                    case "source":
+                        break;
+
+                    case "element":
+                        break;
+                }
+            }
+        }
+        return $jointures;
+    }
+
+    private function _operatorValue($op, $value)
+    {
+        $operator = "";
+        switch ($op) {
+
+            case "null":
+                $operator = "IS NULL ";
+                break;
+
+                //String
+            case "LIKE %var%":
+                $operator = "LIKE '%" . $value . "%' ";
+                break;
+
+            case "NOT LIKE %var%":
+                $operator = "NOT LIKE '%" . $value . "%' ";
+                break;
+
+            case "LIKE var%":
+                $operator = "LIKE '" . $value . "%' ";
+                break;
+
+            case "LIKE %var":
+                $operator = "LIKE '%" . $value . "' ";
+                break;
+
+            case "IS EMPTY":
+                $operator = "IS NULL ";
+
+                //Booleen
+            case "IS TRUE":
+                break;
+
+            case "IS FALSE":
+                $operator = "= 'f' ";
+                break;
+
+            case "IS NULL":
+                $operator = "IS NULL ";
+                break;
+
+                //Liste
+            case "LIKE":
+                $operator = "LIKE '" . $value . "' ";
+                break;
+
+            case "NOT LIKE":
+                $operator = "NOT LIKE '" . $value . "' ";
+                break;
+
+                //Number
+            case ">":
+                $operator = ">" . $value . " ";
+                break;
+
+            case "<":
+                $operator = "<" . $value . " ";
+                break;
+
+            case "=":
+                $operator = "=" . $value . " ";
+                break;
+        }
+        return $operator;
+    }
+
+    private function _traiterRetour($rows, $nomBDD)
+    { //Pour ceux à qui la méthode getNom() ne peut s'appliquer
+        switch ($nomBDD) {
+            case "nom_ville":
+                foreach ($rows as $row) {
+                    if ($row->getNomVille() == "null" || $row->getNomVille() == "") { //Pour virer ce qui n'est pas rempli/null
+
+                    } else {
                         $responseArray[] = array(
                             "nom" => $row->getNomVille()
                         );
-                    }   
+                    }
                 }
                 break;
-              
-            case "nomSite":
-                foreach ($rows as $row) {
-                    if($row->getNomSite() == "null" || $row->getNomSite() == ""){ //Pour virer ce qui n'est pas rempli/null
 
-                    }
-                    else{
+            case "nom_site":
+                foreach ($rows as $row) {
+                    if ($row->getNomSite() == "null" || $row->getNomSite() == "") { //Pour virer ce qui n'est pas rempli/null
+
+                    } else {
                         $responseArray[] = array(
                             "nom" => $row->getNomSite()
                         );
                     }
                 }
                 break;
-            
-            case "etatMorphologique":
-                foreach ($rows as $row) {
-                        $responseArray[] = array(
-                            "nom" => $row->getEtatMorphologique()
-                        );
+
+            case "etat_morphologique":
+                $responseArrayTmp = array();
+
+                foreach ($rows as $row) { //Pour chaque ContientElement
+                    $etatMorpho = $row->getEtatMorphologique();
+                    if ($etatMorpho != null) { //S'il existe
+                        array_push($responseArrayTmp, $etatMorpho);
                     }
+                }
+                foreach ($responseArrayTmp as $row) { //Formattage du tableau pour qu'il corresponde au traitement en JS
+                    $responseArray[] = array(
+                        "nom" => $row
+                    );
+                }
+                break;
+
+            case "theonymes_implicites":
+                $responseArrayTmp = array();
+                foreach ($rows as $row) { //Pour chaque élément
+                    $elem = $row->getTheonymesImplicites(); //Je get le théonyme d'un des élement
+
+                    if (!is_null($elem[0])) //Si l'élément a des théonymes
+                    {
+                        $tabEtatAbsolu = array();
+                        $i = 0;
+
+                        foreach ($elem as $theo) { //Pour chaque théonyme contenu dans l'élement courrant
+                            $tabEtatAbsolu[$i] = strip_tags($theo->getEtatAbsolu()); //On le récupère en enlevant les balises
+                            $i++;
+                        }
+                        $responseArrayTmp = array_merge($responseArrayTmp, $tabEtatAbsolu); //Fusion des deux tableaux
+                    }
+                    $responseArrayTmp = array_unique($responseArrayTmp, SORT_REGULAR); //Suppression des doublons
+                }
+                foreach ($responseArrayTmp as $row) { //Formattage du tableau pour qu'il corresponde au traitement en JS
+                    $responseArray[] = array(
+                        "nom" => $row
+                    );
+                }
                 break;
 
             default:

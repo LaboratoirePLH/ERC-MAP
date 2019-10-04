96059447706ca8355aea87aa7e980218c26ca404
Romain GONÇALVES <romain.goncalves@gmail.com>
2019-10-04 18:00:35 +0200

    Improves sanitization function to remove fonts other than Arial or IFAOGreek

--- src/Entity/AbstractEntity.php
+++ src/Entity/AbstractEntity.php
@@ -14,22 +14,37 @@ abstract class AbstractEntity {
 
     protected function sanitizeWysiwygString($string): ?string
     {
+        // Catch null strings and don't process them
         if(is_null($string)){ return null; }
+
+        // Replace <p> and <div> with a <br/>
         $string = preg_replace("/<p[^>]*?>/", "", $string);
         $string = str_replace("</p>", "<br/>", $string);
         $string = preg_replace("/<div[^>]*?>/", "", $string);
         $string = str_replace("</div>", "<br/>", $string);
+
+        // Clean <br/> tags
         $string = preg_replace("/(<br[^>]*?>)/", "<br/>", $string);
+
+        // Remove all tags except the ones allowed
         $string = strip_tags($string, '<strong><em><u><s><ub><sup><span><br>');
 
         do {
             $before = $string;
+            // Replace ampersand and unbreakable space html entities
             $string = str_replace("&amp;", "&", $string);
             $string = str_replace("&nbsp;", " ", $string);
+            // Remove duplicate <br/>
             $string = preg_replace("/(\s*<br\/>\s*){2,}/", "<br/>", $string);
+            // Remove leading and trailing <br/> and spaces
+            $string = preg_replace("/^(<br\/>)+/", "", $string);
             $string = preg_replace("/(<br\/>)+$/", "", $string);
             $string = trim($string);
         } while($string !== $before); // Repeat until no changes are done
+
+        // Remove fonts other than Arial or IFAOGreek
+        $string = preg_replace('/"font-family:(?!IFAOGreek)[^"]+"/', '"font-family:Arial"', $string);
+
         return $string;
     }
 

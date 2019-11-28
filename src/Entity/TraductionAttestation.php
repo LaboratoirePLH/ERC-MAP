af064d48827e88fcb85fcb28371d7bbfb5116163
Romain GONÇALVES <romain.goncalves@gmail.com>
2019-11-28 18:24:01 +0100

    Increases field length for attestation translation

--- src/Entity/TraductionAttestation.php
+++ src/Entity/TraductionAttestation.php
@@ -13,7 +13,6 @@ use Doctrine\ORM\Mapping as ORM;
 class TraductionAttestation extends AbstractEntity
 {
     use Traits\EntityId;
-    use Traits\TranslatedName;
 
     /**
      * @var \Attestation
@@ -33,6 +32,66 @@ class TraductionAttestation extends AbstractEntity
     public function setAttestation(?Attestation $attestation): self
     {
         $this->attestation = $attestation;
+        return $this;
+    }
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="nom_fr", type="string", length=500, nullable=true)
+     */
+    private $nomFr;
+
+    public function getNomFr(): ?string
+    {
+        return $this->nomFr;
+    }
+
+    public function setNomFr(?string $nomFr): self
+    {
+        $this->nomFr = $nomFr;
+        return $this;
+    }
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="nom_en", type="string", length=500, nullable=true)
+     */
+    private $nomEn;
+
+    public function getNomEn(): ?string
+    {
+        return $this->nomEn;
+    }
+
+    public function setNomEn(?string $nomEn): self
+    {
+        $this->nomEn = $nomEn;
+        return $this;
+    }
+
+    public function getNom(?string $lang): ?string
+    {
+        if($lang == 'fr'){
+            return $this->nomFr;
+        } else {
+            return $this->nomEn;
+        }
+    }
+
+    public function getTranslatedName(): array
+    {
+        return [
+            'nomFr' => $this->getNomFr(),
+            'nomEn' => $this->getNomEn()
+        ];
+    }
+
+    public function setTranslatedName($data): self
+    {
+        $this->setNomFr($data['nomFr'] ?? null);
+        $this->setNomEn($data['nomEn'] ?? null);
         return $this;
     }
 

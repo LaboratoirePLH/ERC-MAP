c9fae1a35be6694e8c2063b99aa57328bda3e87f
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-04-04 20:40:04 +0200

    Fixes topography filter

--- src/Search/Filter/Topographies.php
+++ src/Search/Filter/Topographies.php
@@ -4,7 +4,8 @@ namespace App\Search\Filter;
 
 use App\Entity\IndexRecherche;
 
-class Topographies extends AbstractFilter {
+class Topographies extends AbstractFilter
+{
 
     public static function filter(IndexRecherche $entity, array $criteria, array $sortedData): bool
     {
@@ -14,29 +15,28 @@ class Topographies extends AbstractFilter {
         $localisations = self::resolveLocalisations($entity, $sortedData);
 
         // For each location, we get its topographies IDs
-        $data = array_map(function($el){
-            return array_column($el['topographies'] ?? [], 'id');
-        }, $elements);
+        $data = array_map(function ($l) {
+            return array_column($l['topographies'] ?? [], 'id');
+        }, $localisations);
 
         // For each criteria entry, we will get a boolean result of whether the entry is valid against the data
         // We need at least one truthy value to accept the data
-        return !!count(array_filter(array_map(function($crit) use ($data) {
+        return !!count(array_filter(array_map(function ($crit) use ($data) {
             $requireAll = ($crit['mode'] ?? 'one') === 'all';
             $crit = array_filter($crit['values']);
 
             // We count the matched criteria values
             $matched = 0;
-            foreach($data as $d){
+            foreach ($data as $d) {
                 // $d contains all the topographies linked to a single location
                 // If we require all values to be matched we must find at least as many as the criteria values
                 // Else we only need one
-                if(count(array_intersect($crit, $d)) >= ($requireAll ? count($crit) : 1)){
+                if (count(array_intersect($crit, $d)) >= ($requireAll ? count($crit) : 1)) {
                     return true;
                 }
             }
 
             return false;
-
         }, $criteria)));
     }
 }

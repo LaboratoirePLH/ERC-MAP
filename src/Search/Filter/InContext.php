cd2814dbecd566a7d62be89d6c6d2041c5d559b9
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-04-04 20:35:29 +0200

    Fixes advanced search filter crashing when attestation has no elements

--- src/Search/Filter/InContext.php
+++ src/Search/Filter/InContext.php
@@ -4,7 +4,8 @@ namespace App\Search\Filter;
 
 use App\Entity\IndexRecherche;
 
-class InContext extends AbstractFilter {
+class InContext extends AbstractFilter
+{
 
     public static function filter(IndexRecherche $entity, array $criteria, array $sortedData): bool
     {
@@ -16,23 +17,27 @@ class InContext extends AbstractFilter {
         );
         $data = array_reduce(
             $attestations,
-            function($result, $item){
+            function ($result, $item) {
                 return array_merge(
                     $result,
-                    array_column($item['elements'], "enContexte")
+                    array_column($item['elements'] ?? [], "enContexte")
                 );
             },
             []
         );
         // Remove accents from data and criteria
-        $data     = array_map(function($d){ return \App\Utils\StringHelper::removeAccents($d); }, $data);
-        $criteria = array_map(function($d){ return \App\Utils\StringHelper::removeAccents($d); }, $criteria);
+        $data     = array_map(function ($d) {
+            return \App\Utils\StringHelper::removeAccents($d);
+        }, $data);
+        $criteria = array_map(function ($d) {
+            return \App\Utils\StringHelper::removeAccents($d);
+        }, $criteria);
 
         // For each criteria entry, we will get a boolean result of whether the entry is valid against the data
         // We need at least one truthy value to accept the data
-        return !!count(array_filter(array_map(function($crit) use ($data) {
+        return !!count(array_filter(array_map(function ($crit) use ($data) {
             // We require the criteria value to be present in data
-            return !!count(array_filter($data, function($d) use ($crit){
+            return !!count(array_filter($data, function ($d) use ($crit) {
                 return stristr($d, $crit) !== false;
             }));
         }, $criteria)));

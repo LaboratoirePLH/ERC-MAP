ae82c718a26ad1e876da4f402e085f2dceb23207
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-05-06 15:08:33 +0200

    Adds filter to corpus counting state

--- assets/js/plugins/tree.js
+++ assets/js/plugins/tree.js
@@ -1,5 +1,5 @@
 (function ($) {
-    $.fn.tree = function (data) {
+    $.fn.tree = function (data, filterPlaceholder) {
         var GROUP_ID = 0;
 
         function buildIcon(name) {
@@ -16,16 +16,14 @@
 
             // Add link or text
             if (item.link !== undefined) {
-                li.append($('<a>').attr('href', item.link).attr('target', '_blank').text(item.text));
+                li.append($('<a>').attr('href', item.link).attr('target', '_blank').html(item.text));
             } else {
-                li.text(item.text);
+                li.html(item.text);
             }
 
             // Add badge
             li.append(buildBadge(item.badge))
             li.prepend(buildIcon(item.icon));
-
-            // Indent left
 
             // If item has children, prepare for collapsible
             if (item.children !== undefined && item.children.length) {
@@ -51,11 +49,62 @@
             return li;
         }
 
+        function filter(rootNode, filterValue) {
+            if (filterValue === "") {
+                buildTree(rootNode, data);
+            } else {
+                var filterData = function (dataArray, value) {
+                    var copy = JSON.parse(JSON.stringify(dataArray));
+                    return copy.filter(function (node) {
+                        if (node.children && node.children.length > 0) {
+                            // Not leaf
+                            node.children = filterData(node.children, value);
+
+                            if (node.children.length === 0) {
+                                // If no children left, do not display this node
+                                return false;
+                            } else {
+                                // Recompute badge value
+                                node.badge = node.children.reduce((t, c) => t + c.badge, 0);
+                            }
+                        } else {
+                            // Leaf
+                            const nodeText = $('<div>').html(node.text).text();
+                            return nodeText.toLowerCase().includes(value.toLowerCase());
+                        }
+                        return true;
+                    });
+                }
+                // Filter the data according to the value
+                var filteredData = filterData(data, filterValue);
+                buildTree(rootNode, filteredData);
+            }
+        }
+
+        function buildTree(rootNode, dataArray) {
+            rootNode.children().not('.tree-filter').remove();
+
+            dataArray.forEach(d => {
+                rootNode.append(buildLi(d, 1));
+            });
+        }
+
         return this.each(function () {
             var root = $(this);
-            data.forEach(d => {
-                root.append(buildLi(d, 1));
-            })
+
+            // Search input
+            const searchField = '<div class="input-group">'
+                + '<div class="input-group-prepend">'
+                + '<span class="input-group-text" id="filter-icon-addon">'
+                + buildIcon('filter').removeClass('mr-2').get(0).outerHTML
+                + '</span>'
+                + '</div>'
+                + '<input type="text" id="corpus-state-filter" class="form-control" placeholder="' + filterPlaceholder + '" aria-describedby="filter-icon-addon">'
+                + '</div>';
+            var searchLi = $('<li>').addClass('list-group-item list-group-item-dark tree-filter').append(searchField);
+            root.append(searchLi);
+
+            buildTree(root, data);
 
             root.find('.collapse').on('show.bs.collapse', function () {
                 var id = $(this).prop('id');
@@ -68,6 +117,10 @@
                 var icons = $('span.collapsible-icon[data-target="#' + id + '"]').children();
                 $(icons.get(0)).removeClass('d-none').addClass('d-inline');
                 $(icons.get(1)).removeClass('d-inline').addClass('d-none');
+            });
+            root.find('#corpus-state-filter').on('input', e => {
+                console.log('filter', e.target.value);
+                filter(root, e.target.value);
             });
         });
 

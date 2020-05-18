1d6c554692fe4294e33e873faec2a2018bfb9736
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-05-18 20:39:16 +0200

    Changes source insitu boolean to be managed by the entity instead of the controller, and if the value is true, it sets the origin location to the same value as the discovery location (instead of null)

--- src/Controller/SourceController.php
+++ src/Controller/SourceController.php
@@ -316,9 +316,6 @@ class SourceController extends AbstractController
                     }
                 }
             }
-            if ($source->getInSitu() === true) {
-                $source->setLieuOrigine(null);
-            }
             if ($source->getEstDatee() !== true) {
                 $source->setDatation(null);
             }

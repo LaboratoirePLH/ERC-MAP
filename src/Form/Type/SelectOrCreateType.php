062e9509e2bf7e021ae46f8d5493ceb7814cedc8
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-05-18 16:52:01 +0200

    Fixes class variable being reused between instances

--- src/Form/Type/SelectOrCreateType.php
+++ src/Form/Type/SelectOrCreateType.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
@@ -17,57 +18,39 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class SelectOrCreateType extends AbstractType implements DataMapperInterface
 {
-    private $uuid;
-    private $name;
-    private $entityClass;
-
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $this->name = $options['field_name'];
-        $this->uuid = uniqid($this->name);
-        $this->entityClass = $options['object_class'];
-
         $builder
             ->add("decision", ChoiceType::class, [
-                'label'      => '',
-                'label_attr' => [
-                    'id' => "{$this->name}_decision_{$this->uuid}"
-                ],
-                'attr'     => [
-                    'class' => 'form-check-inline pt-1'
-                ],
-                'mapped'   => false,
-                'expanded' => true,
-                'required' => false,
-                'placeholder' => $options['allow_none'] ? 'generic.choices.none' : false,
-                'choices'  => [
-                    'generic.choices.new' => 'create',
+                'label'       => '',
+                'attr'        => ['class' => 'form-check-inline pt-1'],
+                'mapped'      => false,
+                'expanded'    => true,
+                'required'    => false,
+                'placeholder' => $options['allow_none'] ? $options['none_label'] : false,
+                'choices'     => [
+                    'generic.choices.new'      => 'create',
                     'generic.choices.existing' => 'select',
                 ]
             ])
             ->add("selection", EntityType::class, [
-                'label_attr' => [
-                    'id' => "{$this->name}_selection_{$this->uuid}"
-                ],
                 'required'     => false,
                 'mapped'       => false,
                 'class'        => $options['object_class'],
                 'choice_label' => $options['selection_choice_label'],
                 'attr'         => [
-                    'class' => 'autocomplete',
+                    'class'            => 'autocomplete',
                     'data-placeholder' => $options['translations']['autocomplete.select_element']
                 ],
                 'query_builder' => $options['selection_query_builder']
             ])
             ->add("creation", $options['creation_form_class'], [
-                'label_attr' => [
-                    'id' => "{$this->name}_creation_{$this->uuid}"
-                ],
+                'attr'          => ['class' => $options['creation_form_css_class']],
                 'locale'        => $options['locale'],
                 'translations'  => $options['translations'],
                 'required'      => false,
                 'mapped'        => false,
-                'property_path' => $this->name
+                'property_path' => $options['field_name']
             ]);
 
         $builder->setDataMapper($this);
@@ -78,20 +61,23 @@ class SelectOrCreateType extends AbstractType implements DataMapperInterface
         $resolver->setRequired(['locale', 'translations', 'field_name', 'object_class', 'creation_form_class', 'selection_query_builder', 'selection_choice_label']);
         $resolver->setDefault('locale', 'en');
         $resolver->setDefault('allow_none', false);
+        $resolver->setDefault('none_label', 'generic.choices.none');
         $resolver->setDefault('isClone', false);
         $resolver->setDefault('default_decision', null);
         $resolver->setDefault('formAction', null);
+        $resolver->setDefault('creation_form_css_class', '');
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         $view->vars = array_replace($view->vars, [
-            'field_name' => $this->name,
-            'uuid' => $this->uuid
+            'field_name' => $options['field_name'],
+            'uuid' => uniqid($options['field_name'])
         ]);
     }
 
-    public function getBlockPrefix(){
+    public function getBlockPrefix()
+    {
         return "selectorcreate";
     }
 
@@ -101,13 +87,12 @@ class SelectOrCreateType extends AbstractType implements DataMapperInterface
         $forms = iterator_to_array($forms);
 
         $decision = $forms['decision']->getData();
-        if($decision === "create"){
+        if ($decision === "create") {
             $data = $forms['creation']->getData();
-        }
-        else if ($decision === "select"){
+        } else if ($decision === "select") {
             $data = $forms['selection']->getData();
         }
-        if(empty($data)){
+        if (empty($data)) {
             $data = null;
         }
     }
@@ -128,7 +113,7 @@ class SelectOrCreateType extends AbstractType implements DataMapperInterface
             return;
         }
         $decision = "select";
-        if($forms['decision']->getParent()->getConfig()->getOption('allow_none') === true && $data === null){
+        if ($forms['decision']->getParent()->getConfig()->getOption('allow_none') === true && $data === null) {
             $decision = null;
         }
         $forms['decision']->setData($decision);

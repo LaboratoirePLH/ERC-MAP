d9541db7db2f8d478e117dce2c9405db5d5a8dd3
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-05-27 15:50:00 +0200

    Optimizes source list loading

--- src/Repository/SourceRepository.php
+++ src/Repository/SourceRepository.php
@@ -21,12 +21,52 @@ class SourceRepository extends ServiceEntityRepository
 
     public function findByElement($elementId)
     {
+        return $this->createQueryBuilder("s")
+            ->select("s", "t", "tsu", "m", "ld", "lo", "v", "d", "c", "de", "l", "sb", "b", "tso", "cs")
+            ->leftJoin("s.titrePrincipal", "t")
+            ->leftJoin("s.typeSupport", "tsu")
+            ->leftJoin("s.materiau", "m")
+            ->leftJoin("s.lieuDecouverte", "ld")
+            ->leftJoin("s.lieuOrigine", "lo")
+            ->leftJoin("s.verrou", "v")
+            ->leftJoin("s.datation", "d")
+            ->leftJoin("s.createur", "c")
+            ->leftJoin("s.dernierEditeur", "de")
+            ->leftJoin("s.langues", "l")
+            ->leftJoin("s.sourceBiblios", "sb")
+            ->leftJoin("sb.biblio", "b")
+            ->leftJoin("s.typeSources", "tso")
+            ->leftJoin("tso.categorieSource", "cs")
+            ->leftJoin("s.attestations", "att")
+            ->leftJoin("att.contientElements", "ce")
+            ->leftJoin("ce.element", "e")
+            ->where("e.id = :eId")
+            ->setParameter(":eId", $elementId)
+            ->getQuery()
+            ->getResult();
         $queryBuilder = $this->createQueryBuilder("s");
-        $queryBuilder->leftJoin("s.attestations", "att");
-        $queryBuilder->leftJoin("att.contientElements", "ce");
-        $queryBuilder->leftJoin("ce.element", "e");
-        $queryBuilder->where("e.id = :eId");
-        $queryBuilder->setParameter(":eId", $elementId);
         return $queryBuilder->getQuery()->getResult();
+    }
+
+    public function findAll(): array
+    {
+        return $this->createQueryBuilder("s")
+            ->select("s", "t", "tsu", "m", "ld", "lo", "v", "d", "c", "de", "l", "sb", "b", "tso", "cs")
+            ->leftJoin("s.titrePrincipal", "t")
+            ->leftJoin("s.typeSupport", "tsu")
+            ->leftJoin("s.materiau", "m")
+            ->leftJoin("s.lieuDecouverte", "ld")
+            ->leftJoin("s.lieuOrigine", "lo")
+            ->leftJoin("s.verrou", "v")
+            ->leftJoin("s.datation", "d")
+            ->leftJoin("s.createur", "c")
+            ->leftJoin("s.dernierEditeur", "de")
+            ->leftJoin("s.langues", "l")
+            ->leftJoin("s.sourceBiblios", "sb")
+            ->leftJoin("sb.biblio", "b")
+            ->leftJoin("s.typeSources", "tso")
+            ->leftJoin("tso.categorieSource", "cs")
+            ->getQuery()
+            ->getResult();
     }
 }

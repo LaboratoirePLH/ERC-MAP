b849824058c6854398df1f06837cd4163dd22515
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-06-24 14:03:19 +0200

    Update symfony to 5.1.* and all composer dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 var Encore = require('@symfony/webpack-encore');
 
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
@@ -15,7 +21,7 @@ Encore
      * (including one that's included on every page - e.g. "app")
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     // .createSharedEntry('app', './assets/js/app.js')
     .addEntry('page', './assets/js/page.js')
@@ -28,6 +34,7 @@ Encore
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
+    // I don't want it
     .disableSingleRuntimeChunk()
 
     .copyFiles({
@@ -49,11 +56,21 @@ Encore
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
     // enables Sass/SCSS support
     .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
+
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
     .autoProvidejQuery()
@@ -61,7 +78,7 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
-    ;
+;
 
 var config = Encore.getWebpackConfig();
 

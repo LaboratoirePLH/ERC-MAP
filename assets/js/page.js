e6d4b930f6b504027b845ca66ec4524ca8c2a39e
Romain GONÇALVES <romain.goncalves@gmail.com>
2021-04-13 13:00:18 +0200

    Adds front-end only sorting feature to tables on show pages

--- assets/js/page.js
+++ assets/js/page.js
@@ -2,5 +2,6 @@ require('../css/app.scss');
 
 require('./base.js');
 require('./plugins/tree.js');
+require('./plugins/sorttable.js');
 
 global.counterUp = window.counterUp = require('counterup2').default;

b18511dcb64adb3d074185f06763a78d925c479d
Romain GONÇALVES <romain.goncalves@gmail.com>
2022-03-13 17:33:00 +0100

    Adds inclusive/exclusive criteria in advanced search

--- src/Search/FilterSet/Advanced.php
+++ src/Search/FilterSet/Advanced.php
@@ -42,9 +42,41 @@ class Advanced extends AbstractFilterSet
                     throw new \InvalidArgumentException("Could not find class '$cls' to process criteria $criteriaName");
                 }
 
-                $criteriaAccepted = $cls::filter($e, $criteriaValues, $this->sortedData);
-                if (!$criteriaAccepted) {
-                    continue 2; // Ignore current record, because it did not match the filter
+                // Split criteriaValues between inclusive and exclusive
+                $inclusiveCriteriaValues = [];
+                $exclusiveCriteriaValues = [];
+                foreach ($criteriaValues as $cv) {
+                    $type = 'inclusive';
+                    if (array_key_exists('type', $cv)) {
+                        $type = $cv['type'];
+                        unset($cv['type']);
+                    }
+                    if ($type === 'inclusive') {
+                        $inclusiveCriteriaValues[] = $cv;
+                    } else if ($type === 'exclusive') {
+                        $exclusiveCriteriaValues[] = $cv;
+                    } else {
+                        throw new \InvalidArgumentException("Invalid type value '$cls' for criteria $criteriaName");
+                    }
+                }
+                $inclusiveCriteriaValues = array_filter($criteriaValues, function ($v) {
+                    return ($v['type'] ?? 'inclusive') === 'inclusive';
+                });
+                $exclusiveCriteriaValues = array_filter($criteriaValues, function ($v) {
+                    return ($v['type'] ?? 'inclusive') === 'exclusive';
+                });
+
+                if (count($inclusiveCriteriaValues)) {
+                    $inclusiveCriteriaAccepted = $cls::filter($e, $inclusiveCriteriaValues, $this->sortedData);
+                    if (!$inclusiveCriteriaAccepted) {
+                        continue 2; // Ignore current record, because it did not match the inclusive filter
+                    }
+                }
+                if (count($exclusiveCriteriaValues)) {
+                    $exclusiveCriteriaAccepted = $cls::filter($e, $exclusiveCriteriaValues, $this->sortedData);
+                    if ($exclusiveCriteriaAccepted) {
+                        continue 2; // Ignore current record, because it did match the exclusive filter
+                    }
                 }
             }
 
